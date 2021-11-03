2bfc31d9e127b9ec25b624ab993b6b9e691a2138
Sami-EB <dutinfo123@gmail.com>
2021-11-03 22:07:32 +0100

    Page liste des vins et Page panier + Fonctionnement global du panier

--- client/components/ListeVins.vue
+++ client/components/ListeVins.vue
@@ -1,9 +1,71 @@
 <template>
 	<div>
-		
+		<div class="vin" v-for="vin in vins" :key="vin.id">
+		<div class="vin-img">
+			<div :style="{ backgroundImage: 'url(' + vin.image + ')' }"></div>
+		</div>
+		<div class="vin-content" v-if="editingVin.id !== vin.id">
+		<div class="vin-title">
+			<h2>{{ vin.name }} - {{ vin.prix }}€</h2>
+			<div>
+				<button v-if="panier.vins.find(a => a.id === vin.id) === undefined" @click="addToPanier(vin.id)">Ajouter au panier</button>
+				<button v-else @click="removeFromPanier(vin.id)">Retirer du panier</button>
+			</div>
+		</div>
+		<p>{{ vin.description }}</p>
 	</div>
 </template>
 
 <script>
-
+module.exports = {
+  	props: {
+    	vins: { type: Array, default: [] },
+    	panier: { type: Object }
+  	},
+  	data () {
+    	return {
+			editingVin: {
+				id: -1,
+				name: '',
+				description: '',
+				image: '',
+				prix: 0
+			},
+      	showForm: false
+    	}
+  	},
+  	methods: {
+    	addToPanier (vinId) {
+      		this.$emit('add-to-panier', vinId)
+    	},
+    	removeFromPanier (vinId) {
+      		this.$emit('remove-from-panier', vinId)
+      		this.changerShow(vinId);
+    	}
+  	}
+}
 </script>
+
+<style scoped>
+.vin {
+  	display: flex;
+}
+.vin-img {
+  	flex: 1;
+}
+.vin-img div {
+  	width: 100px;
+  	height: 100px;
+  	background-size: cover;
+}
+.vin-content {
+  	flex: 3;
+}
+.vin-title {
+  	display: flex;
+  	justify-content: space-between;
+}
+textarea {
+  	width: 100%;
+}
+</style>
--- client/components/Panier.vue
+++ client/components/Panier.vue
@@ -1,9 +1,112 @@
 <template>
 	<div>
-
+		<h2>Mon Panier :</h2>
+		<div class="vin" v-for="vin in panier.vins" :key="vin.id">
+			<div class="vin-img">
+				<div :style="{ backgroundImage: 'url(' + vins.find(a => a.id === vin.id).image + ')' }">
+        		</div>
+			</div>
+			<div class="vin-content" v-if="editingVin.id !== vin.id">
+				<p>{{ vin.quantity }}</p>
+				<button @click="editVin(vin)">Modifier</button>
+				<div class="vin-title">
+					<h2>total {{ vins.find(a => a.id === vin.id).name }} : {{ vins.find(a => a.id === vin.id).prix*vin.quantity }}€</h2>
+				</div>
+				<p>{{ vins.find(a => a.id === vin.id).description }}</p>
+			</div>
+			<div class="vin-content" v-else>
+				<div class="vin-title">
+					<h2><input type="number" min="1" v-model="editingVin.quantity"></h2>
+					<div>
+						<button @click="sendEditVin()">Valider</button>
+						<button @click="abortEditVin()">Annuler</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<button @click="updatePrix()">calculer le total</button>
+		<p>Le total est : {{ total }}<p>
 	</div>
 </template>
 
 <script>
-
+module.exports = {
+	props: {
+		vins: { type: Array, default: [] },
+		panier: { type: Object }
+	},
+	data() {
+		return {
+			editingVin: {
+				id: -1,
+				quantity: 0,
+				name: '',
+				description: '',
+				image: '',
+				prix: 0
+			},
+			total: 0
+		};
+	},
+	methods: {
+		editVin (vin) {
+			this.editingVin.id = vin.id
+			this.editingVin.quantity = vin.quantity
+			//this.editingVin.prix = vin.prix
+    	},
+		sendEditVin () {
+			this.$emit('update-panier', this.editingVin)
+			this.abortEditVin()
+		},
+		abortEditVin () {
+			this.editingVin = {
+				id: -1,
+				quantity: 0,
+				name: '',
+				description: '',
+				image: '',
+				prix: 0
+			}
+		},
+		updatePrix(){
+			console.log("test")
+			total = 0
+			this.panier.vins.forEach(vin => {
+				console.log(vin.id)
+				console.log(vin.quantity)
+				console.log(vin.name)
+				console.log(vin.description)
+				console.log(vin.image)
+				console.log(vin.prix)
+				for(i=0;i<vin.quantity;i++){
+					total += vin.prix
+				}
+			})
+		}
+	}
+};
 </script>
+
+<style scoped>
+.vin {
+  	display: flex;
+}
+.vin-img {
+  	flex: 1;
+}
+.vin-img div {
+  	width: 100px;
+  	height: 100px;
+  	background-size: cover;
+}
+.vin-content {
+  	flex: 3;
+}
+.vin-title {
+  	display: flex;
+  	justify-content: space-between;
+}
+textarea {
+  	width: 100%;
+}
+</style>
