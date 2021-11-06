511276a51d401f73e82902217eab0ac83dd3647d
Georges <georges.guitton5@gmail.com>
2021-11-06 23:25:18 +0100

    add histoireVins

--- client/components/HistoireVins.vue
+++ client/components/HistoireVins.vue
@@ -1,21 +1,19 @@
 <template>
     <div class="histoire">
-        <h2>Le petite histoire du vin (sponsorisée par Wikipédia)</h2>
+        <h2 style="text-align:center;">La petite histoire du vin (sponsorisée par Wikipédia)</h2>
         <h4>L’histoire de la vigne et du vin est si ancienne qu’elle se confond avec l'histoire de l’humanité. La vigne et le vin ont représenté un élément important des sociétés, intimement associés à leurs économies et à leurs cultures. Le vin synonyme de fête, d'ivresse, de convivialité, qui a investi le vaste champ des valeurs symboliques, est aujourd'hui présent dans la plupart des pays du monde. Son existence est le fruit d’une longue histoire mouvementée.</h4>
-        
+        <img class="image" src="../assets/vigne-sauvage.jpg"/>
         <h4>La culture de la vigne a été introduite en Gaule par les Grecs de Phocée tandis que le vin a été introduit par les marchands venus des cités étrusques à la fin du VIIe siècle avant notre ère.C'est lors de la création de Massalia (Marseille) aux environs de -600, que les Phocéens implantent la vigne dans la Gaule celtique, les vignobles étant circonscrit à d'étroits espaces proches du littoral. Ce qui a été confirmé par la découverte des premiers vignobles hellénistiques à Saint-Jean-de-Garguier, dans les Bouches-du-Rhône. </h4>
-        
+        <img class="image" src="../assets/vin-grec.jpg"/>
         <h4>À partir du IVe siècle, le christianisme concourt au renforcement de la valeur attachée au vin, prenant la relève d'un Empire romain anéanti. La liturgie de la communion sous les deux espèces (le pain et le vin) pratiquée jusqu’au XIIIe siècle, est l’un des moteurs du maintien de la tradition viticole. Le Moyen Âge se fait le témoin des progrès de qualité du vin. Alors que les vins de l’Antiquité étaient coupés d’eau et agrémentés d’herbes et d’aromates, le vin sous la forme que nous le consommons aujourd'hui, apparaît au Moyen Âge. L’expansion de la civilisation chrétienne est à l’origine de l’expansion de la viticulture dans le monde.</h4>
-        
+        <img class="image" src="../assets/vin-religion.jpg"/>
         <h4>La colonisation des nouveaux mondes se double rapidement d'une expansion de la vigne. Les traces du premier vignoble sud-africain datées de 1659 sont attestées dans la province du Cap, où il a été implanté par les premiers colons. La conquête du Mexique et de toute l’Amérique du Sud, axée sur la diffusion de la religion chrétienne, s'accompagne pareillement d'un développement de la vigne dans ces contrées. On trouve des traces de vignes dans le Piémont andin (Chili central et Argentine) bien que le déploiement d'une industrie viti-vinicole dans ces pays soit dû à des cépages bordelais introduits au Chili au milieu du xvie siècle.En Amérique du Nord, la vigne existe déjà à l’état sauvage, tout particulièrement à l'est, dans l'actuel État de la Virginie, mais aussi au Canada, sous forme de lianes s'enroulant autour des arbres. </h4>
-        
+        <img class="image" src="../assets/vin-nouveau.jpg"/>
         <h4>Jusqu’au XVIIe siècle, le vin constitue la seule boisson stockable et sûre. Ce n’est qu’avec le développement des bières d'une part, l'importation des lointaines colonies du thé, du café et du chocolat faisant apparaître de nouvelles boissons d'autre part, ainsi que le déploiement de l’eau courante, que le vin est détrôné. La fin du xxe siècle est marquée par la concurrence des pays traditionnellement exportateurs (France, Italie) qui privilégient les vins de terroir, et les pays dits du Nouveau Monde (États-Unis, Argentine, Chili, Australie, Afrique-du-Sud) plutôt orientés vers les vins de cépage (appelés aussi vins technologiques).</h4>
     </div>
 </template>
 <style>
-
-.histoire {
-    border-left: solid #a0032a 2px;
-    border-right: solid #a0032a 2px;
+.image {
+    width: 100%;
 }
 </style>
